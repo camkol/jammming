995dfa8649ad638d25eb7760920e916f7ad72c33
camkol <koliyus3@hotmail.com>
2024-06-28 10:53:47 +0200

    start api work

---
+++ src/util/spotify.js
@@ -0,0 +1,11 @@
+let accessToken;
+const clientID = "8c3e2c02caa34e698f8961f59bf0c627";
+const redirectUri = "https://jammming624.netlify.app/callback";
+
+export const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken2;
+    }
+  },
+};
