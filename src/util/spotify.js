15fe092cd2f8248c768cced70fd17213028f03b9
camkol <koliyus3@hotmail.com>
2024-07-26 10:50:26 +0200

    Update spotify.js

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,6 +1,6 @@
 let accessToken;
 const clientID = "8c3e2c02caa34e698f8961f59bf0c627";
-const redirectUri = "https://jammming624.netlify.app/callback";
+const redirectUri = "https://jammming624.netlify.app/";
 
 export const Spotify = {
   getAccessToken() {
